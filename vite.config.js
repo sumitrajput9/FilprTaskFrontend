bd8c4b45437c3f4dc4c07321380e55994c1119c8
SumitR20 <Sumit.Rajput@emossy.com>
2024-11-09 10:44:51 +0530

    update build viteconfig

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          // Split out all node_modules into a separate chunk
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
-        }
-      }
-    },
-    chunkSizeWarningLimit: 1000, // Increase chunk size limit to 1MB to suppress warnings
-  }
 })
